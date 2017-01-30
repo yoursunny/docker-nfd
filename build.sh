a6d213040224d1e7ffad9760bbf74e0dc85652ae
Junxiao Shi <git@mail1.yoursunny.com>
2017-01-30 18:31:46 +0000

    install to clean image

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 VERSION=$(date +%Y%m%d)
-docker build -t nfd-compile:$VERSION .
+
+docker build -t nfd-compile:$VERSION -f Dockerfile-compile .
+docker run --name nfd-install-$VERSION -v $PWD/nfd-install:/target nfd-compile:$VERSION bash -c "
+  cd ndn-cxx && ./waf install --destdir=/target && cd ..;
+  cd NFD && ./waf install --destdir=/target && cd ..;
+  cd ndn-tools && ./waf install --destdir=/target && cd .."
+docker rm nfd-install-$VERSION
+docker build -t nfd-install:$VERSION -f Dockerfile-install .
+sudo rm -rf nfd-install
